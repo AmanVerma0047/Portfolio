d3302ce6a88ab96d174ca7974da10ab9c35b2cd1
AmanVerma0047 <vaman0183@gmail.com>
2025-07-08 22:03:34 +0530

    change social media images import mecahnism

--- src/pages/SocialMedia.jsx
+++ src/pages/SocialMedia.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import './css/SocialMedia.css'
+import email_img from '../assets/email.svg';
+import links_img from '../assets/links.svg';
 
 function SocialMedia() {
     const [isEmail, setisEmail] = useState(false);
@@ -24,8 +26,8 @@ function SocialMedia() {
 
     return <div className="SocialMediaLinks">
         <div className="Icons">
-            <img className="Icons-img" src="src\assets\email.svg" alt="email" onMouseOver={handleEmail} onMouseLeave={nothandleEmail}></img>
-            <img className="Icons-img" src="src\assets\links.svg" alt="email" onMouseOver={handleGithub} onMouseLeave={nothandleGithub} ></img>
+            <img className="Icons-img" src={email_img} alt="email" onMouseOver={handleEmail} onMouseLeave={nothandleEmail}></img>
+            <img className="Icons-img" src={links_img} alt="email" onMouseOver={handleGithub} onMouseLeave={nothandleGithub} ></img>
         </div>
         <div className='Dialogbox'>
             <p className={`EmailText ${isEmail ? 'Email' : 'notisEmail'}`}>Email:vaman0183@gmail.com</p>
