eb94ec4227bb93176712e1483904b59983dd2045
AmanVerma0047 <vaman0183@gmail.com>
2025-07-08 22:59:15 +0530

    improved cards

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,11 +1,9 @@
 import "./css/Card.css"
 
-function Card({image,description}){
+function Card({description}){
     return<>
     <div className="Card-Set">
         <div className="Card-item">
-            {/* <img class="Card-img" height={225} width={225} src={image} alt="Card.."></img> */}
-            {/* <br/> */}
             <p className="Description">{description}</p>
         </div>
     </div>
